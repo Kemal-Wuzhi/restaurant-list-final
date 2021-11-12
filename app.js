acff9344b9b30a851cb342361a980f673a9eb8f3
Kemal-Wuzhi <camelwuzhi@gmail.com>
2021-11-12 21:12:58 +0800

    modify app.js

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
-// const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
 const session = require('express-session')
+const methodOverride = require('method-override')
+// const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
-const app = express()
-const PORT = process.env.PORT
+
 const routes = require('./routes')
-
-
 const usePassport = require('./config/passport')
 require('./config/mongoose')
+
+const app = express()
+const PORT = process.env.PORT
+
+
+
 
 // template engine
 app.engine('handlebars', exphbs({
@@ -22,7 +25,7 @@ app.engine('handlebars', exphbs({
 }))
 app.set('view engine', 'handlebars')
 
-// body parser
+//body parser
 // app.use(bodyParser.urlencoded({
 //   extended: true
 // }))
